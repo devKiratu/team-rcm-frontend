d9ebf6930f53c22899b193917260cfa52e4bff8d
Nameyray <namibaracheal@gmail.com>
2023-01-26 16:29:46 +0300

    about-us

---
+++ src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+function About() {
+  
+  return (
+    <div className='hero'>
+      <div className='text1'> Want to send airtime to your employees?<br></br><br></br>
+       Here's the place to go by only one click<br></br><br></br>
+      <button type='button' class="button" onClick={() => alert("clicked!")}>Send Airtime</button>
+        
+        </div>
+    </div>
+  )
+}
+export default About
